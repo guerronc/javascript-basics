2c15afe33c5f680fe9407da2c8c5e303305e78d9
Cesar Guerron <guerronc@outlook.com>
2019-08-04 09:38:41 -0500

    Agregar ejemplos de funciones con javascript

---
+++ funciones.js
@@ -0,0 +1,12 @@
+var nombre = "cesar",
+  edad = 28;
+
+function imprimirEdad(n, e) {
+    console.log(`${n} have ${e} years old`);   
+}
+
+imprimirEdad(nombre, edad);
+imprimirEdad('Pablo', 36);
+imprimirEdad('Nina', 30);
+
+imprimirEdad(25,'Meli');
---
+++ funciones2.js
@@ -0,0 +1,8 @@
+var nombre = "cesar";
+
+function imprimirNombre(nombre) {
+  nombre = nombre.toUpperCase();
+  console.log(nombre);
+}
+
+imprimirNombre(nombre);
