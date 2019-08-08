6125e957b19340e45fff7dadb63a4d75d4215152
Cesar Guerron <guerronc@outlook.com>
2019-08-07 20:43:55 -0500

    Agregar funciones como parametros

--- clasesProp.js
+++ clasesProp.js
@@ -4,8 +4,12 @@ class Persona {
     this.apellido = apellido;
     this.altura = altura;
   }
-  saludar() {
-    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
+  saludar(fn) {
+    var { nombre, apellido } = this;
+    console.log(`Hola, me llamo ${nombre} ${apellido}`);
+    if (fn) {
+      fn(nombre, apellido);
+    }
   }
   soyAlto() {
     return this.altura > 1.6;
@@ -16,9 +20,25 @@ class Desarrollador extends Persona {
   constructor(nombre, apellido, altura) {
     super(nombre, apellido, altura);
   }
-  saludar() {
-    console.log(
-      `Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrolador`
-    );
+  saludar(fn) {
+    var { nombre, apellido } = this;
+    console.log(`Hola, me llamo ${nombre} ${apellido} y soy desarrolador`);
+    if (fn) {
+      fn(nombre, apellido, true);
+    }
   }
 }
+
+function responderSaludo(nombre, apellido, esDev) {
+  console.log(`Buen dia ${nombre} ${apellido}`);
+}
+
+var cesar = new Persona("Cesar", "Guerron", 1.6);
+var nina = new Persona("Nina", "Zambrano", 1.6);
+var pablo = new Desarrollador("Pablo", "Guerron", 1.6);
+
+cesar.saludar();
+nina.saludar(responderSaludo);
+pablo.saludar(responderSaludo);
+
+// var pablo = new Desarrollador('pablo', 'Guerron', 1.70);
