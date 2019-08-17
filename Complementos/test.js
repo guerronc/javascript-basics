0b55ca6d760d827381d4c7d1a64f2fd01cd5cfe3
Cesar Guerron <guerronc@outlook.com>
2019-08-17 17:17:21 -0500

    Agregar pruebas adicionales para el test

---
+++ Complementos/test.js
@@ -0,0 +1,9 @@
+setTimeout(() => console.log(1), 1000);
+setTimeout(() => console.log(2), 300);
+setTimeout(() => console.log(3), 0);
+console.log(4);
+
+
+var texto = 'Hola mundo'
+
+var primeraLetra = texto.charAt(0)
